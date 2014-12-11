2e0ab3967b3520948d81d6cc00e695323945a1f9
邹晓航 <1210603696@qq.com>
2014-12-11 16:35:46 +0800

    修改usage

--- thread_pool/test.cpp
+++ thread_pool/test.cpp
@@ -34,6 +34,7 @@ int main(){
 			v1.push_back(std::move(ans));
 		}
 		for (int i = 0; i != v.size(); ++i){
+			std::lock_guard<std::mutex> lg(mtx);
 			cout << v[i].get() << endl;
 		}
 		for (int i = 0; i != v1.size(); ++i){
